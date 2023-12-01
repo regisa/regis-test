6069c7291fba130a03642c867ab3b3a105ab8d58
Regis Anclades <regis@anclades.com>
2023-12-01 13:53:59 +0100

    deploy 1

--- src/functions/test1.ts
+++ src/functions/test1.ts
@@ -5,7 +5,7 @@ export async function test1(request: HttpRequest, context: InvocationContext): P
 
     const name = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+    return { body: `test #1, ${name}!` };
 };
 
 app.http('test1', {
